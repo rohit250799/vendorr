dc37fefdc5e3e784930f701e800d762e8263c8f2
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-05-01 22:33:06 +0530

    1. Installed django rest framework in virtual environment 2. Adding api path for vendor urls

--- vendorr/settings.py
+++ vendorr/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my created apps:
+    'vendors',
+    #rest framework:
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
