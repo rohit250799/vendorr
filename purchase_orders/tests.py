65fce3c45ab7f1df8ed6540d9da7f9bb772ed992
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-05-04 21:48:54 +0530

    1. Adding tests for PurchaseOrder ViewSet 2. In the urls file, using a default router and adding url paths for router and on time delivery rate for vendors 3. Creating PurchaseOrder ViewSet in the views file

--- purchase_orders/tests.py
+++ purchase_orders/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from .models import PurchaseOrders
+from vendors.models import Vendor
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIClient
+
+# Order is placed by the buyer before the purchase order is issued to the vendor(order date < issue date)
+class PurchaseOrdersViewSetTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.vendor = Vendor.objects.create(
+            name="Shivam Test Vendor",
+            # contact_details = "Testing contact details",
+            # address = "2 Central Street, Kolkata",
+            # vendor_code = 23,
+            on_time_delivery_rate = 57.33,
+            quality_rating_avg = 34.4,
+            average_response_time = 58,
+            fulfillment_rate = 49.9,
+        )
+
+    def test_on_time_delivery_rate_no_completed_orders(self):
+        #Testing on time delivery rate custom action with 0 completed purchase orders
+        url = reverse('on_time_delivery_rate', args=[self.vendor.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['on_time_delivery_rate'], 0)
+
+    def test_on_time_delivery_rate_with_completed_orders(self):
+        #Testing on time delivery rate custom action with completed purchase orders
+        on_time_purchase_order = PurchaseOrders.objects.create(
+            po_number = 2241,
+            vendor = 2,
+            delivery_date = timezone.now() + timezone.timedelta(days=5),
+            status = "COMPLETED",
+        )
+
+        late_purchase_order = PurchaseOrders.objects.create(
+            po_number = 294,
+            vendor = 2,
+            delivery_date = timezone.now() + timezone.timedelta(days=5),
+            status = "COMPLETED",
+        )
+
+        url = reverse('on_time_delivery_rate', args=[self.vendor.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['on_time_delivery_rate'], 50.0)
+
+    def test_on_time_delivery_rate_invalid_vendor_id(self):
+        #Test the on_time_delivery_rate custom action with an invalid vendor ID.
+        url = reverse('on_time_delivery_rate', args=[])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'error': 'Vendor ID is required.'})
--- purchase_orders/views.py
+++ purchase_orders/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.db.models import F
 from .models import PurchaseOrders
 from .serializers import PurchaseOrderSerializer
-from rest_framework import generics
+from rest_framework import generics, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -12,3 +15,27 @@ class PurchaseOrdersList(generics.ListCreateAPIView):
 class PurchaseOrderDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = PurchaseOrders.objects.all()
     serializer_class = PurchaseOrderSerializer
+
+
+class PurchaseOrderViewSet(viewsets.ModelViewSet):
+    queryset = PurchaseOrders.objects.all()
+    serializer_class = PurchaseOrderSerializer
+
+    @action(detail=False, methods=['get'])
+    def on_time_delivery_rate(self, request, vendor_id=None):
+        if not vendor_id: return Response(
+            {'error': 'Vendor id is required'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+        completed_purchase_order = PurchaseOrders.objects.filter(
+            vendor_id=vendor_id, status='completed')
+        on_time_count = completed_purchase_order.filter(
+            delivery_date__lte=F('delivery_date')).count()
+        total_count = completed_purchase_order.count()
+        if total_count == 0: return Response(
+            {'on_time_delivery_rate': 0}
+        )
+        on_time_delivery_rate = (on_time_count / total_count) * 100
+        return Response({
+            'on_time_delivery_rate': on_time_delivery_rate
+        })
