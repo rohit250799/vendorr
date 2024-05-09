5de5e7c27fe8f78455d93d1cc3e4f734b197419e
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-05-09 16:37:59 +0530

    storing purchase_order as a foreign key from the PurchaseOrders model instead of importing the model

--- purchase_orders/signals.py
+++ purchase_orders/signals.py
@@ -1,15 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import PurchaseOrders
-from vendors.models import Vendor
+#from models import PurchaseOrders
+#from vendors.models import Vendor
 from django.db import models
 
+purchase_order = models.ForeignKey('.PurchaseOrders', on_delete=models.CASCADE)
 
-@receiver(post_save, sender=PurchaseOrders)
+@receiver(post_save, sender=purchase_order)
 def update_quality_rating_average(sender, instance, created, **kwargs):
     if created and instance.quality_rating is not None:
     #if created is not None and instance.quality_rating is not None or 
         vendor = instance.vendor
+        print(vendor)
         completed_purchase_orders = vendor.purchaseorders_set.filter(
             delivery_date__isnull=False,
             status = 'COMPLETE',
